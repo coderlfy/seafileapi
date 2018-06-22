37148ae3b3623f0cf7e2b0f1226af9e5f0a0cf64
coderlfy <13403692778@126.com>
2018-06-22 18:21:55 +0800

    add downloadlink

--- venv/src/entity/Result.py
+++ venv/src/entity/Result.py
@@ -4,11 +4,5 @@ class Result(object):
     success = False
     message = ''
 
-    '''
-    def __init__(self, success, msg):
-        self.success = success
-        self.message = msg
-    '''
-
     def __init__(self):
         self.success = False
