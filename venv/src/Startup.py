425b4ce1832c24c704aee9730dbce381319cb61c
coderlfy <13403692778@126.com>
2018-06-27 17:38:21 +0800

    add downloadlink

--- venv/src/Startup.py
+++ venv/src/Startup.py
@@ -18,7 +18,7 @@ application = tornado.web.Application([
     (r"/SeafileManager/get", handler.SeafileHandler.GetFile),
     (r"/SeafileManager/delete", handler.SeafileHandler.DeleteFile),
     (r"/SeafileManager/clear", handler.SeafileHandler.ClearFile),
-    (r"/SeafileManager/upload", handler.SeafileHandler.UploadLink)
+    (r"/SeafileManager/getuploadurl", handler.SeafileHandler.UploadLink)
 ], **settings)
 if __name__ == "__main__":
     application.listen(28888)
